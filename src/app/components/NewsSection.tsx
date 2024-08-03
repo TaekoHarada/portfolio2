86c16dfb631afe54ef5eaf211c3679e18ac7ef26
taeko harada <taekoharada@taekonoMacBook-Pro.local>
2024-08-03 11:24:15 -0600

    update

--- src/app/components/NewsSection.tsx
+++ src/app/components/NewsSection.tsx
@@ -6,39 +6,26 @@ import NewsCard from "./NewsCard";
 
 import Link from "next/link";
 
-interface NewsItem {
-  title: string;
-  link: string;
-  content: string;
-  id: string;
-}
+const NewsSection: React.FC = () => {
+  const [news, setNews] = useState<{ [key: string]: any }[]>([]);
 
-interface NewsSectionProps {
-  news: NewsItem[];
-}
+  useEffect(() => {
+    const parser = new Parser();
 
-const NewsSection: React.FC<NewsSectionProps> = ({ news }) => {
-  const LIMIT = 5; // Number of items to display
+    const fetchNews = async () => {
+      try {
+        const feed = await parser.parseURL(
+          `https://www.wired.com/feed/category/business/latest/rss`
+        );
+        setNews(feed.items);
+        console.log("Fetched news:", feed.items);
+      } catch (err) {
+        console.error("Error fetching RSS feed:", err);
+      }
+    };
 
-  // const [news, setNews] = useState<{ [key: string]: any }[]>([]);
-
-  // useEffect(() => {
-  //   const parser = new Parser();
-
-  //   const fetchNews = async () => {
-  //     try {
-  //       const feed = await parser.parseURL(
-  //         `https://www.wired.com/feed/category/business/latest/rss`
-  //       );
-  //       setNews(feed.items);
-  //       console.log("Fetched news:", feed.items);
-  //     } catch (err) {
-  //       console.error("Error fetching RSS feed:", err);
-  //     }
-  //   };
-
-  //   fetchNews();
-  // }, []);
+    fetchNews();
+  }, []);
 
   return (
     <section id="TechNews." className="sm:h-screen py-20">
@@ -48,12 +35,12 @@ const NewsSection: React.FC<NewsSectionProps> = ({ news }) => {
         </div>
         <div>TEST</div>
 
-        {news.slice(0, LIMIT).map((item) => (
-          <div key={item.id || item.link}>
+        {news.slice(0, 5).map((item, index) => (
+          <div key={index}>
             <Link href={item.link} target="_blank">
               <div className="text-customGray">
                 <h3 className="project-card__title font-bold text-lg">
-                  {item.title}
+                  Title: {item.title}
                 </h3>
                 <p className="project-card__description mt-3">{item.content}</p>
               </div>
@@ -64,8 +51,8 @@ const NewsSection: React.FC<NewsSectionProps> = ({ news }) => {
         <div className="relative w-full overflow-x-auto">
           <div className="flex group">
             <ul className="animate-loop-scroll flex group-hover:paused">
-              {news.slice(0, LIMIT).map((item) => (
-                <li key={item.id || item.link}>
+              {news.map((item, index) => (
+                <li key={index}>
                   <NewsCard
                     title={item.title}
                     content={item.content}
@@ -76,8 +63,8 @@ const NewsSection: React.FC<NewsSectionProps> = ({ news }) => {
             </ul>
 
             <ul className="animate-loop-scroll flex group-hover:paused">
-              {news.slice(0, LIMIT).map((item) => (
-                <li key={item.id || item.link}>
+              {news.map((item, index) => (
+                <li key={index}>
                   <NewsCard
                     title={item.title}
                     content={item.content}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import AboutSection from "./components/AboutSection";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -8,54 +6,7 @@ import Navbar from "./components/Navbar";
 import NewsSection from "./components/NewsSection";
 import ProjectsSection from "./components/ProjectsSection";
 
-import Parser from "rss-parser";
-import { useEffect, useState } from "react";
-
-// export async function getServerSideProps() {
-//   const parser = new Parser();
-//   const feed = await parser.parseURL(
-//     "https://www.wired.com/feed/category/business/latest/rss"
-//   );
-//   const news = feed.items.map((item) => ({
-//     title: item.title,
-//     link: item.link,
-//     content: item.content,
-//     id: item.id,
-//   }));
-// }
-
-interface NewsItem {
-  title: string;
-  link: string;
-  content: string;
-  id: string;
-}
-
 export default function Home() {
-  const [news, setNews] = useState<NewsItem[]>([]);
-
-  useEffect(() => {
-    const parser = new Parser();
-    const fetchNews = async () => {
-      try {
-        const feed = await parser.parseURL(
-          `https://www.wired.com/feed/category/business/latest/rss`
-        );
-        const newsItems = feed.items.map((item) => ({
-          title: item.title!,
-          link: item.link!,
-          content: item.content!,
-          id: item.id!,
-        }));
-        setNews(newsItems);
-        console.log("Fetched news:", feed.items);
-      } catch (err) {
-        console.error("Error fetching RSS feed:", err);
-      }
-    };
-    fetchNews();
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col bg-gray-50">
       <Navbar />
@@ -63,7 +14,7 @@ export default function Home() {
         <HeroSection />
         <ProjectsSection />
         <AboutSection />
-        <NewsSection news={news} />
+        <NewsSection />
         <Contact />
       </div>
       <Footer />
